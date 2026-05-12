443fd717d83d029fd90d6330c3c76000da2964c8
diego-graziati <dgraziati@gmail.com>
2026-05-12 10:34:51 +0200

    Imported changes to bag recorder and implemented topics_discovery as a way to obtain all available topics in global ros2 to then obtain the topic msg_type by simply specifying the topic's name, without having to otherwise specify the topic's msg_type with its name. Also found a way to obtain the MsgType python class given the msg_type String via rosidl_runtime_py.utilities.get_message() method.

---
+++ topics_discovery.py
@@ -0,0 +1,18 @@
+import time
+from typing import List, Tuple
+import rclpy
+
+class TopicsDiscovery:
+    @staticmethod
+    def get_all_topics() -> List[Tuple[str, List[str]]]:
+        if not rclpy.ok():
+            rclpy.init()
+        
+        discovery_node = rclpy.create_node("discovery_node")
+
+        time.sleep(0.5)
+
+        topics = discovery_node.get_topic_names_and_types()
+
+        discovery_node.destroy_node()
+        return topics
